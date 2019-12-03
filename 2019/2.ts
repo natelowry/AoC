92f355832131353c5efe9fc1881c4ea5dd36e6fa
Nate Lowry <nate.lowry@gmail.com>
2019-12-03 17:10:24 -0600

    yay 2b

--- 2019/2.ts
+++ 2019/2.ts
@@ -73,21 +73,49 @@
 
 const rawInput2 = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,9,23,1,5,23,27,1,27,9,31,1,6,31,35,2,35,9,39,1,39,6,43,2,9,43,47,1,47,6,51,2,51,9,55,1,5,55,59,2,59,6,63,1,9,63,67,1,67,10,71,1,71,13,75,2,13,75,79,1,6,79,83,2,9,83,87,1,87,6,91,2,10,91,95,2,13,95,99,1,9,99,103,1,5,103,107,2,9,107,111,1,111,5,115,1,115,5,119,1,10,119,123,1,13,123,127,1,2,127,131,1,131,13,0,99,2,14,0,0";
 
-var input2 = rawInput2.split(",").map(x => parseInt(x));
+const input2 = rawInput2.split(",").map(x => parseInt(x));
+const input2a = input2.slice();
 var index = 0;
 var current = 0;
-input2[1] = 12;
-input2[2] = 2;
+input2a[1] = 12;
+input2a[2] = 2;
 
 
-while (current != 99) {
-    current = input2[index];
+while (current !== 99) {
+    current = input2a[index];
     if (current === 1) {
-        input2[input2[index + 3]] = input2[input2[index + 1]] + input2[input2[index + 2]];
+        input2a[input2a[index + 3]] = input2a[input2a[index + 1]] + input2a[input2a[index + 2]];
     } else if (current === 2) {
-        input2[input2[index + 3]] = input2[input2[index + 1]] * input2[input2[index + 2]];
+        input2a[input2a[index + 3]] = input2a[input2a[index + 1]] * input2a[input2a[index + 2]];
     }
     index += 4;
 }
 
-console.log(input2[0]);
+console.log(input2a[0]);
+
+while (true) {
+    const r1 = Math.round(Math.random() * 100);
+    const r2 = Math.round(Math.random() * 100);
+
+    const newInput = input2.slice();
+    newInput[1] = r1;
+    newInput[2] = r2;
+    var index = 0;
+    var current = 0;
+
+    while (current !== 99 && index < newInput.length) {
+        current = newInput[index];
+        if (current === 1) {
+            newInput[newInput[index + 3]] = newInput[newInput[index + 1]] + newInput[newInput[index + 2]];
+        } else if (current === 2) {
+            newInput[newInput[index + 3]] = newInput[newInput[index + 1]] * newInput[newInput[index + 2]];
+        }
+        index += 4;
+    }
+
+    const res = newInput[0];
+    if (res === 19690720) {
+        console.log(`${r1}${r2}`);
+        break;
+    }
+}

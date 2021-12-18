3bf40a24d24e7ce6008d357e2c1ecf50ae8c41e9
Nate Lowry <nate.lowry@gmail.com>
2021-12-17 20:15:14 -0600

    and part 2

--- 2021/17.js
+++ 2021/17.js
@@ -1,10 +1,12 @@
 //AOC16a
 var practice = `target area: x=20..30, y=-10..-5`;
 
+var real = 'target area: x=217..240, y=-126..-69';
+
 var input =
   typeof document === "undefined" ||
   document.body.innerText.includes("get your puzzle input")
-    ? practice
+    ? real//practice
     : document.body.innerText.trim();
 
 var targetMinX = Number(input.split("x=")[1].split("..")[0]);
@@ -12,22 +14,30 @@ var targetMaxX = Number(input.split("..")[1].split(",")[0]);
 var targetMinY = Number(input.split("y=")[1].split("..")[0]);
 var targetMaxY = Number(input.split("..")[2]);
 
-console.log(`target: x=${targetMinX}..${targetMaxX}, y=${targetMinY}..${targetMaxY}`);
+console.log(
+  `target: x=${targetMinX}..${targetMaxX}, y=${targetMinY}..${targetMaxY}`
+);
 
 //console.log(hitsTarget(6,9));
 
+var legit = 0;
+
 var maxY = -100000;
 for (var xv = 1; xv < 1000; xv++) {
-  for (var yv = targetMinY; yv < 1000; yv++) {
+  for (var yv = targetMinY; yv < 10000; yv++) {
     var currentMaxY = hitsTarget(xv, yv);
-   // console.log(`${xv},${yv} ${currentMaxY}`);
-    if (currentMaxY > maxY) { 
+    // console.log(`${xv},${yv} ${currentMaxY}`);
+    if (!isNaN(currentMaxY)) {
+      legit++;
+    }
+    if (currentMaxY > maxY) {
       maxY = currentMaxY;
     }
   }
 }
 
 console.log(maxY);
+console.log(legit);
 
 function hitsTarget(xVelocity, yVelocity) {
   var xPos = 0;
@@ -40,7 +50,9 @@ function hitsTarget(xVelocity, yVelocity) {
   while (yPos >= targetMinY && count++ < 1000) {
     xPos += currentXVelocity;
     yPos += currentYVelocity;
-    if (yPos > maxY) { maxY = yPos; }
+    if (yPos > maxY) {
+      maxY = yPos;
+    }
     if (currentXVelocity > 0) {
       currentXVelocity -= 1;
     } else if (currentXVelocity < 0) {
@@ -53,10 +65,10 @@ function hitsTarget(xVelocity, yVelocity) {
       yPos >= targetMinY &&
       yPos <= targetMaxY
     ) {
-     // console.log(`HIT! ${xPos},${yPos}`);
+      // console.log(`HIT! ${xPos},${yPos}`);
       return maxY;
     }
-   // console.log(`${xPos},${yPos} (${currentXVelocity},${currentYVelocity})`);
+    // console.log(`${xPos},${yPos} (${currentXVelocity},${currentYVelocity})`);
   }
 
   // console.log("MISS!");

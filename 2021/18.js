4e9291d12ddca3ab87daf80e11b662cfd52ad69b
Nate Lowry <nate.lowry@gmail.com>
2021-12-18 05:21:18 -0600

    18b bruuuuuut

--- 2021/18.js
+++ 2021/18.js
@@ -13,6 +13,17 @@ practice = `[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
 [[[5,[7,4]],7],1]
 [[[[4,2],2],6],[8,7]]`;
 
+practice = `[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]`;
+
 // practice = `[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
 // [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
 // [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
@@ -64,6 +75,19 @@ function getMagnitude(m) {
   return (3*a) + (2*b);
 }
 console.log(getMagnitude(currentValue));
+
+//b
+var largestMag = 0;
+var linesB = input.split("\n");
+for (var i = 0; i < linesB.length; i++) {
+  for (var j = 0; j < linesB.length; j++) {
+    if (i == j) { continue; }
+    var magnitude = getMagnitude(reduce(add(strToArr(linesB[i]),strToArr(linesB[j]))));
+    largestMag = Math.max(largestMag, magnitude);
+  }
+}
+
+console.log(`largest: ${largestMag}`);
 
 function strToArr(x) {
   return x && JSON.parse(x);

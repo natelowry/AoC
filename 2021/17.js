f01395ecc3b2223b6e875825d86110cfbacf9c8d
Nate Lowry <nate.lowry@gmail.com>
2021-12-17 20:10:36 -0600

    part 1

---
+++ 2021/17.js
@@ -0,0 +1,64 @@
+//AOC16a
+var practice = `target area: x=20..30, y=-10..-5`;
+
+var input =
+  typeof document === "undefined" ||
+  document.body.innerText.includes("get your puzzle input")
+    ? practice
+    : document.body.innerText.trim();
+
+var targetMinX = Number(input.split("x=")[1].split("..")[0]);
+var targetMaxX = Number(input.split("..")[1].split(",")[0]);
+var targetMinY = Number(input.split("y=")[1].split("..")[0]);
+var targetMaxY = Number(input.split("..")[2]);
+
+console.log(`target: x=${targetMinX}..${targetMaxX}, y=${targetMinY}..${targetMaxY}`);
+
+//console.log(hitsTarget(6,9));
+
+var maxY = -100000;
+for (var xv = 1; xv < 1000; xv++) {
+  for (var yv = targetMinY; yv < 1000; yv++) {
+    var currentMaxY = hitsTarget(xv, yv);
+   // console.log(`${xv},${yv} ${currentMaxY}`);
+    if (currentMaxY > maxY) { 
+      maxY = currentMaxY;
+    }
+  }
+}
+
+console.log(maxY);
+
+function hitsTarget(xVelocity, yVelocity) {
+  var xPos = 0;
+  var yPos = 0;
+  var currentXVelocity = xVelocity;
+  var currentYVelocity = yVelocity;
+  var maxY = -123412341234;
+  //console.log(`${xPos},${yPos} (${currentXVelocity},${currentYVelocity})`);
+  var count = 0;
+  while (yPos >= targetMinY && count++ < 1000) {
+    xPos += currentXVelocity;
+    yPos += currentYVelocity;
+    if (yPos > maxY) { maxY = yPos; }
+    if (currentXVelocity > 0) {
+      currentXVelocity -= 1;
+    } else if (currentXVelocity < 0) {
+      currentXVelocity += 1;
+    }
+    currentYVelocity -= 1;
+    if (
+      xPos >= targetMinX &&
+      xPos <= targetMaxX &&
+      yPos >= targetMinY &&
+      yPos <= targetMaxY
+    ) {
+     // console.log(`HIT! ${xPos},${yPos}`);
+      return maxY;
+    }
+   // console.log(`${xPos},${yPos} (${currentXVelocity},${currentYVelocity})`);
+  }
+
+  // console.log("MISS!");
+  return NaN;
+}
